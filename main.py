71dbe8d629cb1e78b15aef6c376cb46d16ca1e8d
余明明 <709410814@qq.com>
2023-04-11 17:19:37 +0800

    增加了语音播报和保存名单的功能

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton,  QPlainTex
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QDir, Qt
 import win32com.client as win32
+from datetime import datetime
+import pyttsx3
 
 studentNum = 0
 
@@ -45,9 +47,16 @@ class Stats():
     def RollCall(self):
         global studentNum
         
+        #显示在左边三栏上
         self.ui.studentName.setPlainText(student_id_list[studentNum][2].value)
         self.ui.studentClass.setPlainText(student_id_list[studentNum][0].value)
         self.ui.studentID.setPlainText(student_id_list[studentNum][1].value)
+        
+        #播放被点到的人的姓名
+        speaker.say(student_id_list[studentNum][2].value)
+        speaker.runAndWait()
+        
+        #更新右边表格数据
         self.ui.tableWidget.insertRow(0)
         nameItem = QTableWidgetItem("%s" % student_id_list[studentNum][2].value)
         classItem = QTableWidgetItem("%s" % student_id_list[studentNum][0].value)
@@ -61,6 +70,7 @@ class Stats():
         #还需要实现按学号排序
         
     def Sort(self):
+        #bug:排序后再点旷课后，不是显示在应该显示的学生表格上
         self.ui.tableWidget.sortItems(2,Qt.AscendingOrder)
     
     def Absenteeism(self):
@@ -76,7 +86,16 @@ class Stats():
     def SaveData(self):
         workbook = openpyxl.load_workbook("Log.xlsx")	# 返回一个workbook数据类型的值
         sheet = workbook.active
-        sheet['G2'] = 1
+        sheet.insert_cols(7)
+        sheet['G2'] = datetime.now().strftime('%Y-%m-%d')
+        
+        tableRow = self.ui.tableWidget.rowCount()
+        for i in range(tableRow):
+            tableName = self.ui.tableWidget.item(i,0).text()
+            for row in range(3,49):
+                name = sheet.cell(row ,3).value
+                if name == tableName:
+                    sheet.cell(row, 7, self.ui.tableWidget.item(i,3).text())
         workbook.save("Log.xlsx")
         workbook.close()
         
@@ -84,6 +103,10 @@ class Stats():
         os.startfile("Log.xlsx")
     
 
+speaker = pyttsx3.init()
+msg = "你好"
+speaker.say(msg)
+speaker.runAndWait()
 app = QApplication([])
 stats = Stats()
 stats.ui.show()
